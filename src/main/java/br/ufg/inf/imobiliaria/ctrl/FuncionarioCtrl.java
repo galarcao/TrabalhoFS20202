98f517d097421e300f90d13c3cf0fb3ad9d7aabd
Ícaro Aguiar Perez <icaro.aguiar@oobj.com.br>
2021-05-31 20:48:39 -0300

    feat(user): adiciona impl básica para listagem de funcionários

--- src/main/java/br/ufg/inf/imobiliaria/ctrl/FuncionarioCtrl.java
+++ src/main/java/br/ufg/inf/imobiliaria/ctrl/FuncionarioCtrl.java
@@ -1,13 +1,18 @@
 package br.ufg.inf.imobiliaria.ctrl;
 
-
 import br.ufg.inf.imobiliaria.business.FuncionarioBusiness;
+import br.ufg.inf.imobiliaria.ctrl.converter.FuncionarioConverter;
 import br.ufg.inf.imobiliaria.ctrl.dto.FuncionarioResponse;
+import br.ufg.inf.imobiliaria.model.entities.Funcionario;
 import br.ufg.inf.imobiliaria.model.repositories.FuncionarioRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -18,17 +23,15 @@ import java.util.Optional;
 public class FuncionarioCtrl {
 	
 	private final FuncionarioBusiness funcionarioBusiness;
-	private final FuncionarioRepository funcionarioRepository;
 	
 	public FuncionarioCtrl(FuncionarioBusiness funcionarioBusiness, FuncionarioRepository funcionarioRepository) {
 		this.funcionarioBusiness = funcionarioBusiness;
-		this.funcionarioRepository = funcionarioRepository;
 	}
 	
 	@GetMapping
-	public Optional<List<FuncionarioResponse>> listFuncionarios() {
-		
-		return null;
+	public ResponseEntity<Page<FuncionarioResponse>> listFuncionarios(@RequestBody Pageable pageable) {
+		var responsePageable = funcionarioBusiness.list(pageable).map(FuncionarioConverter::toDTO);
+		return ResponseEntity.ok(responsePageable);
 	}
 	
 	@GetMapping("/{id}")
